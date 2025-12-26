7295d74434ab91ce8ae7b953063fcadfc719d2ab
Victor Palha <jv.palha@gmail.com>
2025-12-25 22:03:43 -0300

    feat: connect spring to AWS Cognito

---
+++ src/main/java/com/ashinx/fastdocs/modules/accounts/controllers/ProfileController.java
@@ -0,0 +1,16 @@
+package com.ashinx.fastdocs.modules.accounts.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController()
+public class ProfileController {
+    @GetMapping("/me")
+    public Map<String, Object> execute(@AuthenticationPrincipal Jwt jwt) {
+        return jwt.getClaims();
+    }
+}
